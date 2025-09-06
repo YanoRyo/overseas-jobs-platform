4b4801864b5ec13fb740926160dfa88733462482
RyoYano <>
2025-09-06 17:35:27 +0900

    非推奨コードの修正

--- src/components/SupabaseProvider.tsx
+++ src/components/SupabaseProvider.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/ssr";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
 export function SupabaseProvider({ children }: { children: React.ReactNode }) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const [supabaseClient] = useState(() =>
+    createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  );
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
