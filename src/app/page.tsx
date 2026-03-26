eea4a21377d865d2f29ef59a4d586277771c1de4
RyoYano <yanoryo7032@gmail.com>
2026-03-26 10:52:08 +0900

    fix: force dynamic rendering for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { MentorList } from "@/features/mentors/components/MentorList";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return <MentorList />;
 }
