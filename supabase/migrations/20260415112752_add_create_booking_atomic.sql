0b6aed23e81a6e1960beee6a204f2e88c2eb9a10
yasudamasato <yasudaM84@gmail.com>
2026-04-15 20:33:30 +0900

    create_booking_atomicのEXECUTE権限からanon/authenticatedも剥奪

--- supabase/migrations/20260415112752_add_create_booking_atomic.sql
+++ supabase/migrations/20260415112752_add_create_booking_atomic.sql
@@ -48,6 +48,7 @@ BEGIN
 END;
 $$;
 
--- service_role からのみ呼び出し可能にする（PUBLIC のデフォルト権限も剥奪）
-REVOKE EXECUTE ON FUNCTION create_booking_atomic FROM PUBLIC;
+-- service_role からのみ呼び出し可能にする
+-- PUBLIC のデフォルト権限と ALTER DEFAULT PRIVILEGES による anon/authenticated への自動付与も剥奪
+REVOKE EXECUTE ON FUNCTION create_booking_atomic FROM PUBLIC, anon, authenticated;
 GRANT EXECUTE ON FUNCTION create_booking_atomic TO service_role;
